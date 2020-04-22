93a1a4f1ea00c93b0268319e6d32c9cb8eef9b51
bhavikdodia2000 <dodiabhavik.db@gmail.com>
2020-04-22 15:13:16 +0530

    v1.0

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import chat
 from flask import Flask, jsonify,request
 import time
-app = Flask(__name__);
+app = Flask(__name__)
 @app.route("/bot", methods=["POST"])
 def response():
     query = dict(request.form)['query']
